754e0b071c0e85c9dfb6a15d46856459125ccb39
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-09-05 13:27:35 -0400

    Storage is confusing
    I can't figure out how to do this as storage

--- src/Todo.js
+++ src/Todo.js
@@ -1,86 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
-    constructor(props){
-        super(props);
-
-        this.state={
-            newItem:"",
-            list:[]
-        }
-    }
-    //update react state - should be used for local storage as I get farther in the project
-    updateInput(key, value){
-        this.setState({
-            [key]: value
-        })
-    }
-
-    //unique item with unique id so every time an item is input we can identify different list items
-    addItem(){
-        const newItem={
-            id: 1 + Math.random(),
-            value: this.state.newItem.slice()
-
-        }
-        //copy of list items trying to take and add into list
-        const list = [...this.state.list]
-
-        //this should add new item
-        list.push(newItem);
-
-        //updates state with new list and resets newItem input
-        this.setState({
-            list,
-            newItem:""
-        })
-    }
-    //delete button script
-    deleteItem(id) {
-        //copies list of items
-        const list = [...this.state.list]
-
-        //will grab item clicked by button
-        const updatedList = list.filter(item => item.id !== id)
-
-        this.setState({list: updatedList})
-    }
     render() {
         return (
             <div>
-                Add Item to List 
-                <br/>
-                <input 
-                    type="test"
-                    placeholder="Type item here..."
-                    value={this.state.newItem}
-                    onChange={e => this.updateInput("newItem", e.target.value)}
-                />
-                <button 
-                    onClick={() => this.addItem()}>
-                        Add To List
-                    </button>
-                    <br/>
-                    
-                    <ul>
-                        {this.state.list.map(item => {
-                            //takes in the current state in list - I believe
-                            //this.state.list.map should map over item
-                            return(
-                                //returning the list
-                                <li key={item.id
-                                //needs unique key
-                                }>
-                                    {item.value}
-                                    <button onClick={() => this.deleteItem(item.id)
-                                    //will take item so we can delete item id (double check)
-                                    } >X</button>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                
+                <h3>My To Do List</h3>
             </div>
         )
     }
 }
+
