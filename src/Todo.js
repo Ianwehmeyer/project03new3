e8ce67e7466ec88c74052da919b237491dc10641
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-09-05 12:39:08 -0400

    Update Todo.js
    added script to Todo - error is gone - prepped for info storage as nothing is stored yet. Still need to build
    A. Delete
    B. List
    C. Storage

--- src/Todo.js
+++ src/Todo.js
@@ -9,12 +9,31 @@ export default class Todo extends Component {
             list:[]
         }
     }
+    //update react state - should be used for local storage as I get farther in the project
+    updateInput(key, value){
+        this.setState({
+            [key]: value
+        })
+    }
+
     //unique item with unique id so every time an item is input we can identify different list items
     addItem(){
         const newItem={
             id: 1 + Math.random(),
-            
+            value: this.state.newItem.slice()
+
         }
+        //copy of list items trying to take and add into list
+        const list = [...this.state.list]
+
+        //this should add new item
+        list.push(newItem);
+
+        //updates state with new list and resets newItem input
+        this.setState({
+            list,
+            newItem:""
+        })
     }
     render() {
         return (
